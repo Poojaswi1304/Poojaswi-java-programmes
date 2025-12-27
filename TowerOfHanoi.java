8cc688f762abb02d849c2585d53590a3a18401d6
Poojaswi1304 <poojaswisareddy1304@gmail.com>
2025-12-27 11:02:11 +0530

    Add files via upload

---
+++ TowerOfHanoi.java
@@ -0,0 +1,16 @@
+public class TowerOfHanoi {
+    static void solveHanoi(int n, char source, char helper, char destination) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+        solveHanoi(n - 1, source, helper, destination);
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        solveHanoi(n - 1, helper, destination, source);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        solveHanoi(n, 'A', 'B', 'C');
+    }
+}
